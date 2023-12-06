6f7d3d2261a54413487dab47e41fd5a93e109013
Tolga1999 <tolgadoganer@outlook.com>
2023-12-06 16:26:08 +0100

    creative coding first step

---
+++ app.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+onMounted (() => {
+  let h1 = document.querySelector('h1');
+
+  gsap.to('h1', { opacity: 0});
+
+  let animation = gsap.to("h1", {
+    paused: true,
+    opacity: 1
+  })
+
+  h1.addEventListener('mouseenter', () => animation.play());
+  h1.addEventListener('mouseleave', () => animation.reverse());
+  
+  // gsap.fromTo('h1', { opacity: 0}, { opacity: 1, duration: 5})
+})
+</script>
+
+<template>
+  <h1>Mythical</h1>
+</template>
