4f86343b2a403b36bb5c06c0a71467002391c497
Tolga1999 <tolgadoganer@outlook.com>
2023-12-07 16:37:34 +0100

    added heading

--- app.vue
+++ app.vue
@@ -5,29 +5,12 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 onMounted(() => {
-  gsap.set('.cursor', {
-    xPercent: -50,
-    yPercent: -50,
-    scale: 0
-  });
-
-  let h1 = document.querySelector('h1');
-
-  gsap.to('h1', { opacity: 0 });
-
-  let animation = gsap.to("h1", {
-    paused: true,
-    opacity: 1
-  })
-
-  // h1.addEventListener('mouseenter', () => animation.play());
-  // h1.addEventListener('mouseleave', () => animation.reverse());
-
-  // gsap.fromTo('h1', { opacity: 0}, { opacity: 1, duration: 5})
+  gsap.fromTo('h1', { opacity: 0}, { opacity: 1, duration: 3})
 })
 </script>
 
 <template>
-  <div class="cursor"></div>
-  <h1>Mythical</h1>
+  <section>
+      <h1>Partners Overzicht</h1>
+  </section>
 </template>
