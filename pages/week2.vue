dfa2c312a19fa13ba6def1f9040059b6285a034b
Tolga1999 <tolgadoganer@outlook.com>
2023-12-12 22:40:41 +0100

    week 2

---
+++ pages/week2.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <article>YO</article>
+        <article>YO</article>
+        <article>YO</article>
+        <article>YO</article>
+    </section>
+</template>
+
+<style scoped>
+section {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+}
+
+article{
+    border: 2px solid red;
+}
+</style>
