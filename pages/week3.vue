40d4f5af15be5cf7a53b467be9a75d802b9c1d2c
Tolga1999 <tolgadoganer@outlook.com>
2023-12-19 16:47:19 +0100

    made a centered circle

---
+++ pages/week3.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <div class="circle"></div>
+    </main>
+</template>
+
+<style scoped>
+main{
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.circle{
+    background-color: red;
+    width: 25em;
+    height: 25em;
+    border-radius: 50%;
+    /* clip-path: circle(50% at 50% 50%); */
+}
+</style>
